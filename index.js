3b063932ddc9de90eaef658e551b0a176058754d
marts-dev <8363978+marts-dev@users.noreply.github.com>
2024-04-05 04:22:03 +0800

    Route 1 implementation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,14 +9,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
-
+const PRIVATE_APP_ACCESS = process.env.HUBSPOT_KEY;
+const baseUrl = 'https://api.hubspot.com'
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
+app.get('/', async (req, res) => {
+    const cars = `${baseUrl}/crm/v3/objects/cars?limit=10&properties=model&properties=make&properties=color`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(cars, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Cars | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+})
 // * Code for Route 1 goes here
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
+app.get('/update-cobj', async (req, res) => {
+    
+})
 // * Code for Route 2 goes here
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
