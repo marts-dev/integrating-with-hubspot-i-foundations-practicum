2f5b2566ad42222e73b7d3d594e69978ac09bda1
marts-dev <8363978+marts-dev@users.noreply.github.com>
2024-04-07 03:23:33 +0800

    Practicum Implementation

--- index.js
+++ index.js
@@ -13,15 +13,18 @@ const PRIVATE_APP_ACCESS = process.env.HUBSPOT_KEY;
 const baseUrl = 'https://api.hubspot.com'
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 app.get('/', async (req, res) => {
-    const cars = `${baseUrl}/crm/v3/objects/cars?limit=10&properties=model&properties=make&properties=color`;
+    const cars = `${baseUrl}/crm/v3/objects/cars?limit=10&properties=model&properties=make&properties=color&properties=vin&properties=year`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     }
+
+    const errorMessage = req.query.error;
+    const successMessage = req.query.success;
     try {
         const resp = await axios.get(cars, { headers });
         const data = resp.data.results;
-        res.render('homepage', { title: 'Cars | HubSpot APIs', data });      
+        res.render('homepage', { title: 'Cars | HubSpot APIs', error: errorMessage, success: successMessage, data });      
     } catch (error) {
         console.error(error);
     }
@@ -30,57 +33,53 @@ app.get('/', async (req, res) => {
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 app.get('/update-cobj', async (req, res) => {
-    
-})
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    const cars = `${baseUrl}/crm/v3/objects/cars?limit=10&properties=model&properties=make&properties=color&properties=vin&properties=year`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     }
     try {
-        const resp = await axios.get(contacts, { headers });
+        const resp = await axios.get(cars, { headers });
         const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });      
     } catch (error) {
         console.error(error);
     }
-});
+})
+// * Code for Route 2 goes here
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post('/update-cobj', async (req, res) => {
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
     }
+    try {
+        const cars = `${baseUrl}/crm/v3/objects/cars`;
+        const resp = await axios.post(cars, {properties: req.body}, { headers });
+        res.redirect("/?success="+encodeURIComponent("Car successfully created."));      
+    }catch (error) {
+        console.error(error.message);
+        res.redirect("/?error="+encodeURIComponent(error.message+": Failed to create car."));
+    }
+})
 
-});
-*/
+app.post('/update-cobj/:id', async (req, res) => {
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const cars = `${baseUrl}/crm/v3/objects/cars/${req.params.id}`;
+        const resp = await axios.patch(cars, {properties: req.body}, { headers });
+        res.redirect("/?success="+encodeURIComponent("Car successfully updated."));      
+    }catch (error) {
+        console.error(error);
+        res.redirect("/?error="+encodeURIComponent(error.message+": Failed to update car."));
+    }
+})
+// * Code for Route 3 goes here
+
 
 
 // * Localhost
